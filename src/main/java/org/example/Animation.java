c9eee5927daa3e82c3f01e5cec54a506f43e9f1a
adamdab <adamdabkowski01@gmail.com>
2023-02-07 11:52:34 +0100

    refactor-2
    Extracted light management into LightManager

--- src/main/java/org/example/Animation.java
+++ src/main/java/org/example/Animation.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import org.example.window.Window;
+import org.example.manager.LightManager;
 import org.example.controller.SpaceShip;
 import org.example.controller.Satellite;
 import org.example.controller.Neptune;
@@ -33,7 +34,6 @@ import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.IntStream;
-import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -47,19 +47,13 @@ public class Animation {
     static Camera camera = new Camera();
     static float deltaTime = 0.0f;    // Time between current frame and last frame
     static float lastTime = 0.0f; // Time of last frame
-    static float spotLightRotation = -5f;
-    static float fogFactor = 0f;
-    static Vector3f worldUp = new Vector3f(0, 1, 0);
-    private static final List<PointLight> pointLights = new ArrayList<>();
-    private static final List<SpotLight> spotLights = new ArrayList<>();
-    private static final List<DirectionalLight> directionalLights = new ArrayList<>();
-    private static final Vector3f directionalLightColor = new Vector3f(0.15f, 0.15f, 0.15f);
     private static CameraMovementType cameraMovementType = TPV;
 
     private static Window window = new Window();
     private static SpaceShip spaceShip;
     private static Satellite satellite;
     private static Neptune neptune;
+    private static LightManager lightManager;
     public static void main(String[] args) throws IOException {
 
         window.init();
@@ -67,15 +61,12 @@ public class Animation {
             System.out.println("Window initialization failed!");
             return;
         }
-        // set up lights and fog
-        setupLights();
 
-        Fog fog = Fog.builder()
-                .color(directionalLightColor)
-                .build();
+        lightManager = new LightManager();
+        lightManager.init();
 
-        spaceShip = new SpaceShip(spotLights.get(1), spotLights.get(0));
-        satellite = new Satellite(spotLights.get(2));
+        spaceShip = new SpaceShip(lightManager.getSpotLight(1), lightManager.getSpotLight(0));
+        satellite = new Satellite(lightManager.getSpotLight(2));
         neptune = new Neptune();
 
         float frame = 0;
@@ -87,12 +78,13 @@ public class Animation {
         // Begin animation
         while (!window.shouldBeClosed()) {
 
-            // frames
+            // frames per second calculation
+            float currentTime = (float) glfwGetTime();
             frame+= deltaTime * 200;
             framesInSecond ++;
-            float currentTime = (float) glfwGetTime();
             deltaTime = currentTime - lastTime;
             lastTime = currentTime;
+
             if (currentTime - prevFullSecondTime >= 1f) {
                 window.setWindowTitle(framesInSecond);
                 framesInSecond = 0;
@@ -125,99 +117,39 @@ public class Animation {
             }
 
             window.cleanFrame();
-
-            final var view = camera.getViewMatrix();
-            final var projection = new Matrix4f()
-                    .perspective((float) toRadians(camera.getFov()), 1920.0f / 1080.0f, 0.1f, 100.0f);
-
             Shader currentShader = window.getCurrentShader();
 
             currentShader.use();
-            fog.setDensity(fogFactor);
-            fog.applyFog(currentShader);
+            lightManager.useFog(currentShader);
 
             try (MemoryStack stack = MemoryStack.stackPush()) {
 
                 // set up view and projection
+                final var view = camera.getViewMatrix();
+                final var projection = new Matrix4f()
+                        .perspective((float) toRadians(camera.getFov()), 1920.0f / 1080.0f, 0.1f, 100.0f);
+
                 currentShader.setMatrix4fv("view", view.get(stack.mallocFloat(16)));
                 currentShader.setMatrix4fv("projection", projection.get(stack.mallocFloat(16)));
                 currentShader.setVec3("viewPos", camera.getCameraPos());
 
                 // satellite
                 satellite.setStack(stack);
-                satellite.generateFrame(currentShader,spotLightRotation,frame);
+                satellite.generateFrame(currentShader, lightManager.rotation(), frame);
 
                 // spaceShip
                 spaceShip.setStack(stack);
-                spaceShip.generateFrame(currentShader,spotLightRotation,frame);
+                spaceShip.generateFrame(currentShader, lightManager.rotation(), frame);
 
                 // Neptune
                 neptune.setStack(stack);
-                neptune.generateFrame(currentShader,spotLightRotation,frame);
+                neptune.generateFrame(currentShader, lightManager.rotation(), frame);
 
-                // apply uniforms for lights
-                pointLights.forEach(light -> light.applyUniforms(currentShader));
-                spotLights.forEach(light -> light.applyUniforms(currentShader));
-                directionalLights.forEach(light -> light.applyUniforms(currentShader));
-
-                // draw UFOs
-                pointLights.forEach(pointLight -> pointLight.draw(currentShader));
+                lightManager.applyLighting(currentShader);
             }
             window.nextFrame();
         }
         window.delete();
-    }
-
-    private static void setupLights() {
-        Model lamp = new Model(Animation.class.getResource("Low_poly_UFO.obj").getPath());
-        DirectionalLight directionalLight = DirectionalLight.builder()
-                .direction(new Vector3f(0, -1f, 0))
-                .ambient(directionalLightColor)
-                .diffuse(directionalLightColor)
-                .specular(directionalLightColor)
-                .build();
-        directionalLights.add(directionalLight);
-        //  PointLights
-        Vector3f[] pointLightPositions = {
-                new Vector3f(new Vector3f(-8f, 2f, -8f)),
-                new Vector3f(new Vector3f(10f, 0, -10f)),
-                new Vector3f(new Vector3f(10f, 3f, 10f)),
-                new Vector3f(new Vector3f(-7f, 0, 7f)),
-                new Vector3f(new Vector3f(9f, -2f, 0)),
-        };
-        int index = 0;
-        for (var position : pointLightPositions) {
-            PointLight pointLight = PointLight.builder()
-                    .index(index++)
-                    .model(lamp)
-                    .modelPosition(position)
-                    .lightPosition(new Vector3f(0, 1.35f, 0).add(position))
-                    .ambient(new Vector3f(0.005f, 0.005f, 0.005f))
-                    .diffuse(new Vector3f(0.4f, 0.4f, 0.4f))
-                    .specular(new Vector3f(0.1f, 0.1f, 0.1f))
-                    .constant(0.1f)
-                    .linear(0.075f)
-                    .quadratic(0.0175f)
-                    .build();
-            pointLights.add(pointLight);
-        }
-        // SpotLights
-        IntStream.range(0, 3).forEach(i -> {
-            SpotLight spotLight = SpotLight.builder()
-                    .index(i)
-                    .lightPosition(new Vector3f(-10, 1, -5))
-                    .direction(new Vector3f(0, -1f, 0))
-                    .ambient(new Vector3f(0.15f, 0.15f, 0.10f))
-                    .diffuse(new Vector3f(0.3f, 0.3f, 0.2f))
-                    .specular(new Vector3f(0.3f, 0.53f, 0.2f))
-                    .constant(0.1f)
-                    .linear(0.1f)
-                    .quadratic(0.0175f)
-                    .cutOff((float) cos(toRadians(13)))
-                    .outerCutOff((float) cos(toRadians(20)))
-                    .build();
-            spotLights.add(spotLight);
-        });
     }
 
     private static void processInput() {
@@ -235,31 +167,17 @@ public class Animation {
         } else if (window.checkIfPressed(GLFW_KEY_4)) {
             cameraMovementType = FREE;
         } else if (window.checkIfPressed(GLFW_KEY_LEFT_CONTROL) && window.checkIfPressed(GLFW_KEY_UP)) {
-            // fog factor up
-            fogFactor += deltaTime * 0.5f;
-            fogFactor = fogFactor > 1 ? 1 : fogFactor;
+            lightManager.increaseFog(deltaTime);
         } else if (window.checkIfPressed(GLFW_KEY_LEFT_CONTROL) && window.checkIfPressed(GLFW_KEY_DOWN)) {
-            // fog factor down
-            fogFactor -= deltaTime * 0.5f;
-            fogFactor = fogFactor < 0 ? 0 : fogFactor;
+            lightManager.decreaseFog(deltaTime);
         }else if (window.checkIfPressed(GLFW_KEY_LEFT_SHIFT) && window.checkIfPressed(GLFW_KEY_UP)) {
-            // light intensity up
-            float lightIntensity = directionalLightColor.x;
-            lightIntensity += deltaTime * 0.5f;
-            lightIntensity = Math.min(lightIntensity, 0.4f);
-            directionalLightColor.set(lightIntensity, lightIntensity, lightIntensity);
+            lightManager.increaseLightIntensity(deltaTime);
         } else if (window.checkIfPressed(GLFW_KEY_LEFT_SHIFT) && window.checkIfPressed(GLFW_KEY_DOWN)) {
-            // light intensity down
-            float lightIntensity = directionalLightColor.x;
-            lightIntensity -= deltaTime * 0.5f;
-            lightIntensity = Math.max(0, lightIntensity);
-            directionalLightColor.set(lightIntensity, lightIntensity, lightIntensity);
+            lightManager.decreaseLightIntensity(deltaTime);
         } else if (window.checkIfPressed(GLFW_KEY_UP)) {
-            spotLightRotation += deltaTime * 10f;
-            spotLightRotation = spotLightRotation > 30 ? 30 : spotLightRotation;
+            lightManager.liftSpotLights(deltaTime);
         } else if (window.checkIfPressed(GLFW_KEY_DOWN)) {
-            spotLightRotation -= deltaTime * 10f;
-            spotLightRotation = spotLightRotation < -30 ? -30 : spotLightRotation;
+            lightManager.lowerSpotLights(deltaTime);
         }  else if (window.checkIfPressed(GLFW_KEY_P)) {
             window.changeCurrentShader(PHONG);
         }  else if (window.checkIfPressed(GLFW_KEY_G)) {
---
+++ src/main/java/org/example/manager/LightManager.java
@@ -0,0 +1,133 @@
+package org.example.manager;
+
+import org.example.Animation;
+import org.example.lights.*;
+import org.example.shader.Shader;
+import org.example.shader.fog.Fog;
+import org.example.models.Model;
+
+import org.joml.Vector3f;
+import java.util.List;
+import java.util.ArrayList;
+import static java.lang.Math.*;
+
+public class LightManager {
+    private List<PointLight> pointLights = new ArrayList<>();
+    private List<SpotLight> spotLights = new ArrayList<>();
+    private List<DirectionalLight> directionalLights = new ArrayList<>();
+    private Vector3f directionalLightColor = new Vector3f(0.15f, 0.15f, 0.15f);
+
+    private float spotLightRotation = -5f;
+    private float fogFactor = 0f;
+    private Fog fog = Fog.builder().color(directionalLightColor).build();
+
+    public SpotLight getSpotLight(int id) {
+        return spotLights.get(id);
+    }
+
+    public float rotation() {
+        return spotLightRotation;
+    }
+
+    public void init() {
+        Model ufo = new Model(Animation.class.getResource("Low_poly_UFO.obj").getPath());
+        DirectionalLight directionalLight = DirectionalLight.builder()
+                .direction(new Vector3f(0, -1f, 0))
+                .ambient(directionalLightColor)
+                .diffuse(directionalLightColor)
+                .specular(directionalLightColor)
+                .build();
+        directionalLights.add(directionalLight);
+        //  PointLights
+        Vector3f[] pointLightPositions = {
+                new Vector3f(new Vector3f(-8f, 2f, -8f)),
+                new Vector3f(new Vector3f(10f, 0, -10f)),
+                new Vector3f(new Vector3f(10f, 3f, 10f)),
+                new Vector3f(new Vector3f(-7f, 0, 7f)),
+                new Vector3f(new Vector3f(9f, -2f, 0)),
+        };
+        int index = 0;
+        for (var position : pointLightPositions) {
+            PointLight pointLight = PointLight.builder()
+                    .index(index++)
+                    .model(ufo)
+                    .modelPosition(position)
+                    .lightPosition(new Vector3f(0, 1.35f, 0).add(position))
+                    .ambient(new Vector3f(0.005f, 0.005f, 0.005f))
+                    .diffuse(new Vector3f(0.4f, 0.4f, 0.4f))
+                    .specular(new Vector3f(0.1f, 0.1f, 0.1f))
+                    .constant(0.1f)
+                    .linear(0.075f)
+                    .quadratic(0.0175f)
+                    .build();
+            pointLights.add(pointLight);
+        }
+        // SpotLights
+         for(int i = 0; i < 3; i++){
+            SpotLight spotLight = SpotLight.builder()
+                    .index(i)
+                    .lightPosition(new Vector3f(-10, 1, -5))
+                    .direction(new Vector3f(0, -1f, 0))
+                    .ambient(new Vector3f(0.15f, 0.15f, 0.10f))
+                    .diffuse(new Vector3f(0.3f, 0.3f, 0.2f))
+                    .specular(new Vector3f(0.3f, 0.53f, 0.2f))
+                    .constant(0.1f)
+                    .linear(0.1f)
+                    .quadratic(0.0175f)
+                    .cutOff((float) cos(toRadians(13)))
+                    .outerCutOff((float) cos(toRadians(20)))
+                    .build();
+            spotLights.add(spotLight);
+        }
+    }
+
+    public void useFog(Shader currentShader) {
+        fog.setDensity(fogFactor);
+        fog.applyFog(currentShader);
+    }
+
+    public void increaseFog(float dt) {
+        fogFactor += dt * 0.5f;
+        fogFactor = fogFactor > 1 ? 1 : fogFactor;
+    }
+
+    public void decreaseFog(float dt) {
+        fogFactor -= dt * 0.5f;
+        fogFactor = fogFactor < 0 ? 0 : fogFactor;
+    }
+
+    public void increaseLightIntensity(float dt) {
+        float lightIntensity = directionalLightColor.x;
+        lightIntensity += dt * 0.5f;
+        lightIntensity = Math.min(lightIntensity, 0.4f);
+        directionalLightColor.set(lightIntensity, lightIntensity, lightIntensity);
+    }
+
+    public void decreaseLightIntensity(float dt) {
+        float lightIntensity = directionalLightColor.x;
+        lightIntensity -= dt * 0.5f;
+        lightIntensity = Math.max(0, lightIntensity);
+        directionalLightColor.set(lightIntensity, lightIntensity, lightIntensity);
+    }
+
+    public void liftSpotLights(float dt) {
+        spotLightRotation += dt * 10f;
+        spotLightRotation = spotLightRotation > 30 ? 30 : spotLightRotation;
+    }
+
+    public void lowerSpotLights(float dt) {
+        spotLightRotation -= dt * 10f;
+        spotLightRotation = spotLightRotation < -30 ? -30 : spotLightRotation;
+    }
+
+    public void applyLighting(Shader currentShader) {
+
+        // apply uniforms for shaders
+        pointLights.forEach(light -> light.applyUniforms(currentShader));
+        spotLights.forEach(light -> light.applyUniforms(currentShader));
+        directionalLights.forEach(light -> light.applyUniforms(currentShader));
+
+        // draw ufos - lamps
+        pointLights.forEach(pointLight -> pointLight.draw(currentShader));
+    }
+}
